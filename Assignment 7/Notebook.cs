b677207bc9230b485d91442da7ec26bafe522f3c
BONOLO ATONG <bonoloatong@gmail.com>
2022-08-12 15:11:23 +0200

    Add project files.

---
+++ Assignment 7/Notebook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notebook
+
+{
+    public class Notebook
+    {
+        private int _year;
+        private string _brand;
+        private string _OS;
+        private string _processor;
+
+        public Notebook()
+        {
+            this._year = 2016;
+            this._brand = "samsung";
+            this._OS = "Windows";
+            this._processor = "core celeron";
+        }
+        public Notebook(int year, string OS, string processor)
+        {
+            this._year=year;
+            this._OS = OS;
+            this._processor = processor;
+        }
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+        public string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        
+        public string Processor
+        {
+            get { return _processor; }
+            set { _processor = value; }
+        }
+        
+        public string OS
+        {
+            get { return _OS; }
+            set { _OS = value; }
+        }
+        public string GetBrand()
+        {
+            return _brand;
+        }
+        public void SetBrand()
+        {
+            _brand = Brand;
+        }
+
+    }
+
+}
---
+++ Assignment 7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Notebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook Dell = new Notebook(2020, "Windows", "i5");
+            Notebook Lenovo = new Notebook(2021, "Chrome", "i3");
+
+            Console.WriteLine("New Laptop: "+ Dell.Processor);
+            Console.WriteLine("New Lenovo: "+Lenovo.OS);
+        }
+    }
+}
